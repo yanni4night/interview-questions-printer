a164d08b792303738a81aaaea75004548d37119f
yinyong <yinyong@sogou-inc.com>
2014-08-10 22:32:28 +0800

    add service.sh

---
+++ service.sh
@@ -0,0 +1,9 @@
+INTERFACE="bin/www"
+
+if [ "x$1" == 'xstart' ];then
+    forever start $INTERFACE
+elif [ "x$1" == 'xdebug' ];then
+    forever start -c 'supervisor' $INTERFACE
+elif [ "x$1" == 'xstop' ];then
+    forever stop $INTERFACE
+fi
